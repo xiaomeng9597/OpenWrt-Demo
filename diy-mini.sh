e61cdf71ae3fb0a2dfbfdb7150efad6f657a19db
ddnsoft <75249507+ddnsoft@users.noreply.github.com>
2024-03-09 11:11:03 +0800

    Update sed mosdns

--- diy-mini.sh
+++ diy-mini.sh
@@ -68,6 +68,7 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 
 # MosDNS
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/packages/net/mosdns/Makefile
 
 # Alist
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
