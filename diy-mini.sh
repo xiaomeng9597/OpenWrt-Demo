0bacce6af24fbea9995a419047af5b665a18e7af
ddnsoft <75249507+ddnsoft@users.noreply.github.com>
2024-11-07 08:34:35 +0800

    add feeds clean

--- diy-mini.sh
+++ diy-mini.sh
@@ -121,5 +121,8 @@ sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerm
 # 取消对 samba4 的菜单调整
 # sed -i '/samba4/s/^/#/' package/lean/default-settings/files/zzz-default-settings
 
+## Add Start
+./scripts/feeds clean
+## Add End
 ./scripts/feeds update -a
 ./scripts/feeds install -a
